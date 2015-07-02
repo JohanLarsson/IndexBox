630245d624235d41cff26c78299a20c3f014370a
Johan Larsson <johan_gavle1@hotmail.com>
2015-07-02 22:35:24 +0200

    Refactored it slightly.

--- IndexBox/Index.cs
+++ IndexBox/Index.cs
@@ -1,8 +1,5 @@
-using System.Collections.Generic;
-using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Media;
 
 namespace IndexBox
 {
@@ -12,15 +9,15 @@ namespace IndexBox
             "Of",
             typeof(int),
             typeof(Index),
-            new PropertyMetadata(default(int)));
+            new PropertyMetadata(-1));
 
         public static readonly DependencyProperty OfProperty = OfPropertyKey.DependencyProperty;
 
-        public static readonly System.Windows.DependencyProperty InProperty = System.Windows.DependencyProperty.RegisterAttached(
+        public static readonly DependencyProperty InProperty = DependencyProperty.RegisterAttached(
             "In",
-            typeof(ListBox),
+            typeof(ItemsControl),
             typeof(Index),
-            new PropertyMetadata(default(ListBox), OnInChanged));
+            new PropertyMetadata(default(ItemsControl), OnInChanged));
 
         public static void SetOf(this DependencyObject element, int value)
         {
@@ -32,36 +29,26 @@ namespace IndexBox
             return (int)element.GetValue(OfProperty);
         }
 
-        public static void SetIn(this DependencyObject element, ListBox value)
+        public static void SetIn(this DependencyObject element, ItemsControl value)
         {
             element.SetValue(InProperty, value);
         }
 
-        public static ListBox GetIn(this DependencyObject element)
+        public static ItemsControl GetIn(this DependencyObject element)
         {
-            return (ListBox)element.GetValue(InProperty);
+            return (ItemsControl)element.GetValue(InProperty);
         }
 
         private static void OnInChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
-            if (e.NewValue == null)
+            var itemsControl = e.NewValue as ItemsControl;
+            if (itemsControl == null)
             {
-                d.SetOf(-1);
                 return;
             }
-            var item = d.Ancestors().OfType<ListBoxItem>().First();
-            var listBox = (ListBox) e.NewValue;
-            d.SetOf(listBox.ItemContainerGenerator.IndexFromContainer(item));
-        }
-
-        public static IEnumerable<DependencyObject> Ancestors(this DependencyObject o)
-        {
-            DependencyObject parent = VisualTreeHelper.GetParent(o);
-            while (parent != null)
-            {
-                yield return parent;
-                parent = VisualTreeHelper.GetParent(parent);
-            }
+            var itemContainerGenerator = itemsControl.ItemContainerGenerator;
+            var indexFromContainer = itemContainerGenerator.IndexFromContainer(d);
+            d.SetOf(indexFromContainer);
         }
     }
 }
